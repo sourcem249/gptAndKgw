bfbcd23e7110cfc0eb803bd985d65254dcff7672
source <sourcem249@gmail.com>
2025-10-31 16:04:18 +0900

    앱 구동을 위한 파일 설치1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,18 @@
-rootProject.name = "WeatherApp"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "gptAndKgw"
 include(":app")
